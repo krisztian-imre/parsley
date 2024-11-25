9ba280e0ab4f7816dd391e1f38689b80d3c7b2fe
krisztian-imre <krisztian.imre@icloud.com>
2024-11-25 17:19:04 +0100

    Bash file

--- gateio_set_all_processed.py
+++ gateio_set_all_processed.py
@@ -7,9 +7,9 @@ filename = 'gateio_article_collection.tsv'
 df = pd.read_csv(filename, sep='\t')
 
 # Set all 'llm_processed' values to 'Yes'
-df['llm_processed'] = 'Yes'
+df['llm_processed'] = 'No'
 
 # Save the updated DataFrame back to the TSV file
 df.to_csv(filename, sep='\t', index=False)
 
-print("Updated 'llm_processed' to 'Yes' for all records.")
+print("Updated 'llm_processed' to all records.")
